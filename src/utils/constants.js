b5205a2c7ff0798a052563c6dac4f89823105da8
Rameez <rameezraja275@gmail.com>
2020-09-30 17:52:38 +0500

    push notification

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export default {
-  VERSION: "1.3.3",
+  VERSION: "1.4.1",
   APP_NAME: "eHisaab",
   OFFICE_ADDRESS: "Top Floor ZS Plaza Jutial Gilgit",
   POWERED_BY: "Pine Technologies (Pvt) Ltd",
